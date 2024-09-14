8db29ddd90c3404f4044eaaeb7fb564e513f7a0d
janStulhofer <janstulhofer8@post.cz>
2024-09-14 19:25:34 +0200

    config pro vlastni barvy a tridu text glow

--- chronoAlchemy/tailwind.config.ts
+++ chronoAlchemy/tailwind.config.ts
@@ -4,8 +4,31 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				grayMpBg: '#191C1F',	//Šedá na hlavní stránku
+			},
+			textShadow: {
+				'glow': '0 0 10px rgba(255, 255, 255, 0.8)',
+			},
+		},
+		fontFamily: {
+
+		}
 	},
 
-	plugins: []
+	mode: 'jit', // just-in-time mod, rychlejší vykreslování CSS
+
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				'.text-shadow-glow': {
+					'text-shadow': '0 0 10px rgba(255, 255, 255, 0.8)',
+				},
+			};
+			addUtilities(newUtilities, ['responsive', 'hover']);
+		}
+	],
+
+	
 } as Config;
