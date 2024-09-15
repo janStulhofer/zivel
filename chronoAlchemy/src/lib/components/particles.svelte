5973fc118d2372f0b491d8ee6cc75ca0f59e1084
janStulhofer <janstulhofer8@post.cz>
2024-09-15 19:32:55 +0200

    Implementace particles.js do aplikace

---
+++ chronoAlchemy/src/lib/components/particles.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    // Přidání <div> do <body>
+      const div = document.createElement('div');
+    div.className = "absolute min-h-screen top-0 left-0 w-full z-[-1] opacity-70";
+    div.innerHTML = `
+      <div class="absolute inset-0">
+        <div id="particles-js" class="absolute inset-0"></div>
+      </div>
+    `;
+    document.body.appendChild(div);
+
+    // Přidání <script> do <body> pro načtení particles.js konfigurace
+    const scriptBody = document.createElement('script');
+    scriptBody.textContent = `
+			particlesJS.load("particles-js", "assets/particlesjs-config.json", function () {
+				console.log("callback - particlesjs-config.js config loaded");
+			});
+    `;
+    document.body.appendChild(scriptBody);
+  });
+</script>
--- chronoAlchemy/src/routes/+page.svelte
+++ chronoAlchemy/src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<div class=" bg-grayMpBg h-screen items-center justify-center flex">
+<script>
+    // Import komponenty particles.js 
+    import Particles from '$lib/components/particles.svelte';
+  </script>
+
+<Particles />
+<div class="  h-screen items-center justify-center flex">
 	<div class=" text-white text-center my-auto">
         <p class="text-shadow-glow">Toto je Chrono Alchemy!</p>
     </div>
