92c732938d9876b0042379c8e9f955c36cbc4c8a
janStulhofer <janstulhofer8@post.cz>
2024-09-15 19:32:19 +0200

    Implementace particles,js pozadí

--- chronoAlchemy/src/routes/+page.svelte
+++ chronoAlchemy/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class=" text-green-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class=" bg-grayMpBg h-screen items-center justify-center flex">
+	<div class=" text-white text-center my-auto">
+        <p class="text-shadow-glow">Toto je Chrono Alchemy!</p>
+    </div>
+</div>
