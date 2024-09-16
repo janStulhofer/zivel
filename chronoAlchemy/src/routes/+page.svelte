e54899e8ff891663d7dc1fc2759832ad667ed847
janStulhofer <janstulhofer8@post.cz>
2024-09-16 21:49:00 +0200

    Přidání headeru, font

--- chronoAlchemy/src/routes/+page.svelte
+++ chronoAlchemy/src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     // Import komponenty particles.js 
     import Particles from '$lib/components/particles.svelte';
+    import Header from '$lib/components/Header.svelte';
   </script>
 
 <Particles />
+<Header />
 <div class="  h-screen items-center justify-center flex">
-	<div class=" text-white text-center my-auto">
-        <p class="text-shadow-glow">Toto je Chrono Alchemy!</p>
+	<div class=" text-white text-center my-auto backdrop-blur-xs">
+        <p class="text-shadow-glow font-PTSerif text-2xl m-20">Toto je Chrono Alchemy!</p> 
     </div>
 </div>
