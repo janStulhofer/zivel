6234d743df886a606f2c76b2ab02b3e231539ba0
janStulhofer <janstulhofer8@post.cz>
2024-09-21 16:39:47 +0200

    Opravené animace

--- chronoAlchemy/src/routes/registrace/+page.svelte
+++ chronoAlchemy/src/routes/registrace/+page.svelte
@@ -6,7 +6,7 @@
 <div class="container mx-auto p-4">
 	<button
 		class="text-shadow-glow border bg-slate-950 transition hover:shadow-3xl hover:invert font-PTSerif mt-16 rounded-2xl border-white px-3 py-1 text-xl font-bold text-white duration-300 ease-in-out lg:px-4 lg:py-2 lg:text-2xl"
-		on:click={() => navigateWithTransition('/', 'slide-right')}
+		on:click={() => navigateWithTransition('/', 'slide-left')}
 	>
 		Zpět
 	</button>
