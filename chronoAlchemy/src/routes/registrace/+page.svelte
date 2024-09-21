1c5b1f77aa976a912aa945b206304b466daf6e94
janStulhofer <janstulhofer8@post.cz>
2024-09-21 17:21:04 +0200

    Test registrace

--- chronoAlchemy/src/routes/registrace/+page.svelte
+++ chronoAlchemy/src/routes/registrace/+page.svelte
@@ -1,9 +1,46 @@
 <!-- src/routes/registrace/+page.svelte -->
 <script>
 	import { navigateWithTransition } from '$lib/transitions';
+	import { supabase } from '$lib/supabaseClient';
+	import { goto } from '$app/navigation';
+
+let email = '';
+let password = '';
+let error = '';
+
+async function handleSubmit() {
+  const { data, error: signUpError } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (signUpError) {
+    error = signUpError.message;
+  } else {
+    goto('/login');
+  }
+}
 </script>
 
 <div class="container mx-auto p-4">
+	<form on:submit|preventDefault={handleSubmit}>
+		<div>
+		  <label for="email">Email:</label>
+		  <input type="email" id="email" bind:value={email} required />
+		</div>
+		<div>
+		  <label for="password">Heslo:</label>
+		  <input type="password" id="password" bind:value={password} required />
+		</div>
+		<button type="submit">Registrovat</button>
+	  </form>
+	  
+	  {#if error}
+		<p class="error">{error}</p>
+	  {/if}
+
+
+
 	<button
 		class="text-shadow-glow border bg-slate-950 transition hover:shadow-3xl hover:invert font-PTSerif mt-16 rounded-2xl border-white px-3 py-1 text-xl font-bold text-white duration-300 ease-in-out lg:px-4 lg:py-2 lg:text-2xl"
 		on:click={() => navigateWithTransition('/', 'slide-left')}
