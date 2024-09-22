8e6c8e4679dfb6fb5755338f56ac8dd08c9301ee
janStulhofer <janstulhofer8@post.cz>
2024-09-22 21:10:46 +0200

    Úprava registrační stránky

--- chronoAlchemy/src/routes/registrace/+page.svelte
+++ chronoAlchemy/src/routes/registrace/+page.svelte
@@ -1,8 +1,8 @@
-<!-- src/routes/registrace/+page.svelte -->
 <script>
 	import { navigateWithTransition } from '$lib/transitions';
 	import { supabase } from '$lib/supabaseClient';
 	import { goto } from '$app/navigation';
+	import Particles from '$lib/components/particles.svelte';
 
 let email = '';
 let password = '';
@@ -22,9 +22,11 @@ async function handleSubmit() {
 }
 </script>
 
-<div class="container mx-auto p-4">
+<Particles />
+
+<div class="container mx-auto p-4 text-white items-center justify-center flex my-5 pt-10">
 	<form on:submit|preventDefault={handleSubmit}>
-		<div>
+		<div class="pb-6">
 		  <label for="email">Email:</label>
 		  <input type="email" id="email" bind:value={email} required />
 		</div>
@@ -32,7 +34,11 @@ async function handleSubmit() {
 		  <label for="password">Heslo:</label>
 		  <input type="password" id="password" bind:value={password} required />
 		</div>
-		<button type="submit">Registrovat</button>
+		<button type="submit" class="text-shadow-glow border bg-slate-950 transition hover:shadow-3xl hover:invert font-PTSerif mt-16 rounded-2xl border-white px-3 py-1 text-xl font-bold text-white duration-300 ease-in-out lg:px-4 lg:py-2 lg:text-2xl"
+		on:click={() => navigateWithTransition('#', 'slide-left')}
+		>
+	Registrovat
+</button>
 	  </form>
 	  
 	  {#if error}
