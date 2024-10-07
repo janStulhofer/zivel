edccdd5960433a76d5c0adc4daf363ffdf048b3e
janStulhofer <janstulhofer8@post.cz>
2024-10-07 22:04:24 +0200

    Formátování

--- chronoAlchemy/src/lib/components/Header.svelte
+++ chronoAlchemy/src/lib/components/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+  	import { navigateWithTransition } from '$lib/transitions';
 </script>
 
 <header class="p-4 text-white">
@@ -9,17 +10,22 @@
       <nav class="mt-4 lg:mt-0 lg:col-start-3 lg:justify-self-end">
         <ul class="flex flex-wrap justify-center lg:justify-end">
           <li class="m-2">
-            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif">
+            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif"
+            on:click={() => navigateWithTransition('/registrace', 'slide-right')}
+            >
               Registrace
             </button>
           </li>
           <li class="m-2">
-            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif">
+            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif"
+            on:click={() => navigateWithTransition('/login', 'slide-right')}
+            >
               Přihlásit se
             </button>
           </li>
           <li class="m-2">
-            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif">
+            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif"
+            >
               O stránce
             </button>
           </li>
