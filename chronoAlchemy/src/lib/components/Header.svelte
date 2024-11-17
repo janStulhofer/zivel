3dc1614f57b3926380b4ed831295431d081bd621
janStulhofer <janstulhofer8@post.cz>
2024-11-17 18:19:29 +0100

    Nové logo + font fix

--- chronoAlchemy/src/lib/components/Header.svelte
+++ chronoAlchemy/src/lib/components/Header.svelte
@@ -5,26 +5,26 @@
 <header class="p-4 text-white">
     <div class="flex flex-col items-center lg:grid lg:grid-cols-3 lg:items-center">
       <div class="lg:col-start-2 lg:justify-self-center">
-        <img src="/assets/logo2.png" class="w-[300px] lg:w-[500px] invert" alt="Chrono Alchemy" />
+        <img src="/assets/logoZivel-White.png" class="w-[300px] lg:w-[500px]" alt="Chrono Alchemy" />
       </div>
       <nav class="mt-4 lg:mt-0 lg:col-start-3 lg:justify-self-end">
         <ul class="flex flex-wrap justify-center lg:justify-end">
           <li class="m-2">
-            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif"
+            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-Rubik"
             on:click={() => navigateWithTransition('/registrace', 'slide-right')}
             >
               Registrace
             </button>
           </li>
           <li class="m-2">
-            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif"
+            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-Rubik"
             on:click={() => navigateWithTransition('/login', 'slide-right')}
             >
               Přihlásit se
             </button>
           </li>
           <li class="m-2">
-            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-PTSerif"
+            <button class="text-shadow-glow-xl rounded-2xl border border-white bg-slate-950 px-3 py-1 text-sm text-white transition duration-300 ease-in-out hover:shadow-3xl hover:invert lg:px-4 lg:py-2 lg:text-2xl font-Rubik"
             >
               O stránce
             </button>
