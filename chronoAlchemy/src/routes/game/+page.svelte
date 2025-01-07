7e1a7e741c678bfad1bcec7f3855e6bbbab24376
janStulhofer <janstulhofer8@post.cz>
2025-01-07 15:11:00 +0100

    kontrola zda byl tutorial jiz splnen pro zabraneni opetovnemu nacitani vyberu avatara

--- chronoAlchemy/src/routes/game/+page.svelte
+++ chronoAlchemy/src/routes/game/+page.svelte
@@ -22,7 +22,8 @@
   let loading = true;
   let tutorialCompleted = false;
   let currentAvatarId = 0;
-  let showAvatarSelection = true;
+  let showAvatarSelection = false; 
+  let dataLoaded = false; 
   
   onMount(async () => {
     const { data: { user } } = await supabase.auth.getUser();
@@ -37,85 +38,75 @@
       if (data) {
         tutorialCompleted = data.tutorial;
         currentAvatarId = data.avatar_id;
+        showAvatarSelection = !data.tutorial;
       }
     } else {
       navigateWithTransition('/login', 'slide-right');
     }
     loading = false;
+    dataLoaded = true;
   });
   
-  async function handleLogout() {
-    await supabase.auth.signOut();
-    navigateWithTransition('/login', 'slide-right');
-  }
-  
   async function updateAvatar(newAvatarId: number) {
-  const { data: { user } } = await supabase.auth.getUser();
-  if (user) {
-    const { data, error } = await supabase
-      .from('user_profiles')
-      .update({ avatar_id: newAvatarId, tutorial: true }) 
-      .eq('id', user.id);
-    
-    if (!error) {
-      currentAvatarId = newAvatarId;
-      tutorialCompleted = true;
-      showAvatarSelection = false;
-    } else {
-      console.error('Error updating avatar:', error);
+    const { data: { user } } = await supabase.auth.getUser();
+    if (user) {
+      const { data, error } = await supabase
+        .from('user_profiles')
+        .update({ avatar_id: newAvatarId, tutorial: true })
+        .eq('id', user.id);
+      
+      if (!error) {
+        currentAvatarId = newAvatarId;
+        tutorialCompleted = true;
+        showAvatarSelection = false;
+      } else {
+        console.error('Error updating avatar:', error);
+      }
     }
   }
-}
   
   function handleKeyDown(event: KeyboardEvent, avatarId: number) {
     if (event.key === 'Enter' || event.key === ' ') {
       updateAvatar(avatarId);
     }
   }
-  </script>
-  
-  <Particles />
-  
-  <div class="container mx-auto p-4 text-white items-center justify-center flex flex-col my-5 pt-10">
-    {#if loading}
-      <p class="text-2xl font-bold">Načítání...</p>
-      <img src="/assets/loading2.gif" alt="nacitani">
-    {:else}
-      <h1 class="text-4xl font-bold mb-8">Vítejte ve hře, <span class="text-white font-extrabold">{username}</span>!</h1>
-      
-      {#if tutorialCompleted}
-        <p class="text-xl mb-8">Vítejte zpět! Zde je váš aktuální stav hry.</p>
-        <!-- přidat hraci pole etc... nebo predelat na /tutorial /game? -->
-      {:else if showAvatarSelection}
-        <p class="text-xl mb-8" transition:fade>Začneme krátkým úvodem do hry, teď si vyberte svůj avatar!</p>
-      {/if}
+</script>
+
+<Particles />
+
+<div class="container mx-auto p-4 text-white items-center justify-center flex flex-col my-5 pt-10">
+  {#if loading}
+    <p class="text-2xl font-bold">Načítání...</p>
+    <img src="/assets/loading2.gif" alt="nacitani">
+  {:else if dataLoaded}
+    <h1 class="text-4xl font-bold mb-8">Vítejte ve hře, <span class="text-white font-extrabold">{username}</span>!</h1>
+    
+    {#if tutorialCompleted}
+      <p class="text-xl mb-8">Vítejte zpět! Zde je váš aktuální stav hry.</p>
+    {:else if showAvatarSelection}
+      <p class="text-xl mb-8" transition:fade>Začneme krátkým úvodem do hry, teď si vyberte svůj avatar!</p>
     {/if}
-  </div>
-  
-  {#if !tutorialCompleted && showAvatarSelection}
-    <div class="container mx-auto px-4 max-w-2xl" transition:fly="{{ y: 300, duration: 300 }}">
-      <div class="grid grid-cols-4 grid-rows-2 gap-4">
-        {#each avatars as avatar, i}
-          <div 
-            class="aspect-square rounded-lg overflow-hidden cursor-pointer hover:ring-2 hover:ring-white transition-all {currentAvatarId === i ? 'ring-4 ring-slate-600' : ''}"
-            on:click={() => updateAvatar(i)}
-            on:keydown={(e) => handleKeyDown(e, i)}
-            role="button"
-            tabindex="0"
-          >
-            <img
-              src={avatar}
-              alt="Avatar option {i + 1}"
-              class="w-full h-full object-cover"
-            />
-          </div>
-        {/each}
-      </div>
-    </div>
   {/if}
-  
-  <!--
-  Opravit zobrazovani avatar menu pri refresh stranky nebo prechodu na stranku i po dokonceni tutorialu
-  Mozna rozdelit Tutorial stranku a Game stranku na jednotlive
-  Opravit chybu s vyberem Avataru1 a Avataru2
-  -->
+</div>
+
+{#if showAvatarSelection && dataLoaded} <!-- Přidána podmínka dataLoaded -->
+  <div class="container mx-auto px-4 max-w-2xl" transition:fly="{{ y: 300, duration: 300 }}">
+    <div class="grid grid-cols-4 grid-rows-2 gap-4">
+      {#each avatars as avatar, i}
+        <div
+          class="aspect-square rounded-lg overflow-hidden cursor-pointer hover:ring-2 hover:ring-white transition-all {currentAvatarId === i ? 'ring-4 ring-slate-600' : ''}"
+          on:click={() => updateAvatar(i)}
+          on:keydown={(e) => handleKeyDown(e, i)}
+          role="button"
+          tabindex="0"
+        >
+          <img
+            src={avatar}
+            alt="Avatar option {i + 1}"
+            class="w-full h-full object-cover"
+          />
+        </div>
+      {/each}
+    </div>
+  </div>
+{/if}
