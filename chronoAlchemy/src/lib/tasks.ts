fabd6537bc71c0fc02ed035862d056c47da143a8
janStulhofer <janstulhofer8@post.cz>
2025-02-03 14:13:51 +0100

    Seznam úkolů

---
+++ chronoAlchemy/src/lib/tasks.ts
@@ -0,0 +1,24 @@
+export const TASKS = [
+    {
+      id: 1,
+      title: "První kroky",
+      description: "Kombinuj Oheň a Vodu pro vytvoření Páry",
+      target: "steam",
+      reward: 5,
+    },
+    {
+      id: 2,
+      title: "Není tu zima?",
+      description: "Zahřej se trochu!",
+      target: "lava",
+      reward: 5,
+    },
+    {
+      id: 3,
+      title: "Až moc horko...",
+      description: "Chtělo by to zase ochladit...",
+      target: "ice",
+      reward: 5,
+    },
+  ];
+  
