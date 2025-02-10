41241945ccf0f4e178d3e5a786db48d0641febe7
janStulhofer <janstulhofer8@post.cz>
2025-02-10 20:38:01 +0100

    Komponenta pro koš

---
+++ chronoAlchemy/src/lib/components/TrashCan.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { trashStore } from '$lib/stores/trashStore';
+  
+    let trashZone: HTMLElement;
+  
+    onMount(() => {
+  const updatePosition = () => {
+    const rect = trashZone.getBoundingClientRect();
+    trashStore.set({
+      x: rect.left + rect.width/2,
+      y: rect.top + rect.height/2,
+      radius: 50 
+    });
+  };
+  
+  updatePosition();
+  window.addEventListener('resize', updatePosition);
+  return () => window.removeEventListener('resize', updatePosition);
+});
+  </script>
+  
+  <div class="absolute left-4 bottom-4 z-50" bind:this={trashZone}>
+    <img
+      src="/assets/gui/trash-can.png"
+      alt="Koš"
+      class="w-12 h-12 transition-all duration-200 hover:scale-110"
+    />
+  </div>
