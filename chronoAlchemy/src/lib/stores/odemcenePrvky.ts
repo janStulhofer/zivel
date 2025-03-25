b2ad27c5bfb6b1de48ea0cf5bdf7bab1a71dc977
janStulhofer <janstulhofer8@post.cz>
2025-03-25 20:27:25 +0100

    Čeština

--- chronoAlchemy/src/lib/stores/odemcenePrvky.ts
+++ chronoAlchemy/src/lib/stores/odemcenePrvky.ts
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store';
 
-export const unlockedElements = writable(['water', 'air', 'fire', 'earth']);
+export const unlockedElements = writable(['Voda', 'Vzduch', 'Oheň', 'Země']);
